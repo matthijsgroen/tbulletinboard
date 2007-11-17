4fe34b095de8fd58ce8b754a9be1f68cab341a7a
matthijs.groen <matthijs.groen@161cc63b-ca3e-0410-92ac-71db65a8b288>
2007-11-17 11:31:11 +0000

    Source voorzien van de GPL3 licentie notificatie

--- trunk/tbulletinboard/classes/MemberGroups.class.php
+++ trunk/tbulletinboard/classes/MemberGroups.class.php
@@ -1,11 +1,23 @@
 <?php
 	/**
-	 * THAiSies Bulletin Board
-	 * 2003 Rewrite
+	 *	TBB2, an highly configurable and dynamic bulletin board
+	 *	Copyright (C) 2007  Matthijs Groen
 	 *
-	 *@author Matthijs Groen (thaisi at servicez.org)
-	 *@version 2.0
+	 *	This program is free software: you can redistribute it and/or modify
+	 *	it under the terms of the GNU General Public License as published by
+	 *	the Free Software Foundation, either version 3 of the License, or
+	 *	(at your option) any later version.
+	 *	
+	 *	This program is distributed in the hope that it will be useful,
+	 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *	GNU General Public License for more details.
+	 *	
+	 *	You should have received a copy of the GNU General Public License
+	 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *	
 	 */
+
 	require_once($TBBclassDir . "Group.bean.php");
 
 	class MemberGroup {
