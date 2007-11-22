65faf3bf1941dc46d1fd2c327330b6f21b1ce262
Matthijs Groen <matthijs.groen@gmail.com>
2007-11-22 12:06:25 +0000

    Init patches van de module gemaakt

---
+++ tbulletinboard/modules/messageSystem/patches/ptch1195730809 - message-init.php
@@ -0,0 +1,57 @@
+<?php
+	/**
+	 *	TBB2, an highly configurable and dynamic bulletin board
+	 *	Copyright (C) 2007  Matthijs Groen
+	 *
+	 *	This program is free software: you can redistribute it and/or modify
+	 *	it under the terms of the GNU General Public License as published by
+	 *	the Free Software Foundation, either version 3 of the License, or
+	 *	(at your option) any later version.
+	 *	
+	 *	This program is distributed in the hope that it will be useful,
+	 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *	GNU General Public License for more details.
+	 *	
+	 *	You should have received a copy of the GNU General Public License
+	 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *	
+	 */
+
+	$patchName = "message-init";
+	$patchFunc = false; // false by no function, name of function otherwise
+	$patchAuthor = "Matthijs Groen"; // 100 = IV, 131 = Matthijs, 120 = Guido, 126 = Urvin
+
+?>
+-- phpMyAdmin SQL Dump
+-- version 2.10.3deb1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Nov 22, 2007 at 12:26 PM
+-- Server version: 5.0.45
+-- PHP Version: 5.2.3-1ubuntu6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- Database: `tbb2`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tbb_message_global`
+-- 
+
+CREATE TABLE IF NOT EXISTS `tbb_message_global` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `settingID` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `tbb_message_global`
+-- 
+
+
---
+++ tbulletinboard/modules/referenceTopic/patches/ptch1195730561 - reference-init.php
@@ -0,0 +1,60 @@
+<?php
+	/**
+	 *	TBB2, an highly configurable and dynamic bulletin board
+	 *	Copyright (C) 2007  Matthijs Groen
+	 *
+	 *	This program is free software: you can redistribute it and/or modify
+	 *	it under the terms of the GNU General Public License as published by
+	 *	the Free Software Foundation, either version 3 of the License, or
+	 *	(at your option) any later version.
+	 *	
+	 *	This program is distributed in the hope that it will be useful,
+	 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *	GNU General Public License for more details.
+	 *	
+	 *	You should have received a copy of the GNU General Public License
+	 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *	
+	 */
+
+	$patchName = "reference-init";
+	$patchFunc = false; // false by no function, name of function otherwise
+	$patchAuthor = "Matthijs Groen"; // 100 = IV, 131 = Matthijs, 120 = Guido, 126 = Urvin
+
+?>
+-- phpMyAdmin SQL Dump
+-- version 2.10.3deb1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Nov 22, 2007 at 12:22 PM
+-- Server version: 5.0.45
+-- PHP Version: 5.2.3-1ubuntu6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- Database: `tbb2`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tbb_tm_referencetopic`
+-- 
+
+CREATE TABLE IF NOT EXISTS `tbb_tm_referencetopic` (
+  `topicID` bigint(20) unsigned NOT NULL default '0',
+  `type` enum('topic','board','url') NOT NULL default 'topic',
+  `newWindow` enum('yes','no') NOT NULL default 'no',
+  `value` varchar(255) NOT NULL default '',
+  `created` enum('user','system') NOT NULL default 'user',
+  PRIMARY KEY  (`topicID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- 
+-- Dumping data for table `tbb_tm_referencetopic`
+-- 
+
+
