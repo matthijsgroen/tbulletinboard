92ffcffd77ca284c74cbde3d6fba47b20dfbce6d
matthijs.groen <matthijs.groen@161cc63b-ca3e-0410-92ac-71db65a8b288>
2007-11-25 21:41:37 +0000

    Installatie wizard toegevoegd!

---
+++ trunk/tbulletinboard/classes/interface/Text.class.php
@@ -0,0 +1,57 @@
+<?php
+	/**
+	 *	TBB2, an highly configurable and dynamic bulletin board
+	 *	Copyright (C) 2007  Matthijs Groen
+	 *
+	 *	This program is free software: you can redistribute it and/or modify
+	 *	it under the terms of the GNU General Public License as published by
+	 *	the Free Software Foundation, either version 3 of the License, or
+	 *	(at your option) any later version.
+	 *	
+	 *	This program is distributed in the hope that it will be useful,
+	 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *	GNU General Public License for more details.
+	 *	
+	 *	You should have received a copy of the GNU General Public License
+	 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *	
+	 */
+
+	class Text {
+
+		var $privateVars;
+
+		function Text() {
+			$this->privateVars = array();
+			$this->privateVars['text'] = array();
+		}
+
+		function addHTMLText($text) {
+			$this->privateVars['text'][] = array('type' => 'html', 'content' => $text);
+		}
+
+		function addHTMLheader($text) {
+			$this->privateVars['text'][] = array('type' => 'htmlheader', 'content' => $text);
+		}
+
+		function showText() {
+?>
+		<div class="center">
+<?php
+			for ($i = 0; $i < count($this->privateVars['text']); $i++) {
+				$textPart = $this->privateVars['text'][$i];
+				if ($textPart['type'] == 'html') {
+					print "<div class=\"text\"><p>".$textPart['content']."</p></div>";
+				}
+				if ($textPart['type'] == 'htmlheader') {
+					print "<h3 class=\"textheader\">".$textPart['content']."</h3>";
+				}
+			}
+?>
+		</div>
+<?php
+		}
+	}
+
+?>
