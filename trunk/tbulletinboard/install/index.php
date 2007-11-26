363d7baa6504d3462fab8ade23373012bea10159
matthijs.groen <matthijs.groen@161cc63b-ca3e-0410-92ac-71db65a8b288>
2007-11-26 20:06:30 +0000

    schrijfrechten check aangepast

--- trunk/tbulletinboard/install/index.php
+++ trunk/tbulletinboard/install/index.php
@@ -35,6 +35,27 @@
 	includeFormComponents("TextField", "TemplateField", "PlainText", "ButtonBar", "Submit", "PasswordField", "NumberField", 
 		"EmailField", "Checkbox");
 	
+	function is__writable($path) {
+		//will work in despite of Windows ACLs bug
+		//NOTE: use a trailing slash for folders!!!
+		//see http://bugs.php.net/bug.php?id=27609
+		//see http://bugs.php.net/bug.php?id=30931
+
+		if ($path{strlen($path)-1}=='/') // recursively return a temporary file path
+		    return is__writable($path.uniqid(mt_rand()).'.tmp');
+		else if (is_dir($path))
+		    return is__writable($path.'/'.uniqid(mt_rand()).'.tmp');
+		// check tmp file for read/write capabilities
+		$rm = file_exists($path);
+		$f = @fopen($path, 'a');
+		if ($f===false)
+		    return false;
+		fclose($f);
+		if (!$rm)
+		    unlink($path);
+		return true;
+	}
+	
 	function getForm($step) {
 		global $formTitleTemplate;
 		$maxStep = 5;
@@ -79,7 +100,7 @@
 			$notWriteable = array();
 			
 			for ($i = 0; $i < count($folderCheck); $i++) {
-				if (!is_writable("../upload/".$folderCheck[$i])) {
+				if (!is__writable("../upload/".$folderCheck[$i])) {
 					$notWriteable[] = $folderCheck[$i];
 				}
 			}
